10a7a0b914693da951abf610c957381998ef3640
nam3886 <43193144+nam3886@users.noreply.github.com>
2023-02-25 10:37:31 +0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen h-full relative bg-gray-100 pb-20">
+    <Navbar />
+    <div class="fixed bottom-0 w-full">
+      <AuthenticationStatus />
+    </div>
+    <Welcome />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: '🔐 nuxt-auth-example',
+})
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    css: [
+        '@fortawesome/fontawesome-free/css/all.min.css'
+    ],
+    modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
+    auth: {
+        origin: process.env.ORIGIN,
+        enableGlobalAppMiddleware: true
+    }
+})
